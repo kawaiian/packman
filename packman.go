14e07abf896a255e6f3315df695ffc0b1505762a
kawaiian <kawaiian@users.noreply.github.com>
2019-01-08 21:36:15 -0800

    Added regexp to handleRequest().

--- packman.go
+++ packman.go
@@ -4,18 +4,20 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"log"
 	"net"
 	"os"
+	"regexp"
 	"strings"
 	"sync"
 )
 
 const (
-	host     = "0.0.0.0"
-	port     = "8080"
 	addr     = host + ":" + port
 	connType = "tcp"
+	host     = "0.0.0.0"
+	port     = "8080"
 )
 
 var (
@@ -23,10 +25,10 @@ var (
 	pkgTree map[string][]string
 )
 
-type Request struct {
-	request      string
-	pkg          string
-	dependencies []string
+type pkgRequest struct {
+	req         string
+	pkg         string
+	depndencies []string
 }
 
 //--- main ---
@@ -39,6 +41,7 @@ func main() {
 	defer lstnr.Close()
 	log.Print("Packman server listening on " + addr + "...")
 
+	// Accept connections, concurrently
 	for {
 		conn, err := lstnr.Accept()
 		if err != nil {
@@ -54,26 +57,36 @@ func handleRequest(c net.Conn) {
 	log.Printf("Serving %s\n", c.RemoteAddr().String())
 
 	for {
-		req, err := bufio.NewReader(c).ReadString('\n')
+		msg, err := bufio.NewReader(c).ReadString('\n')
 		if err != nil {
 			log.Printf("Error reading request: %v\n", err)
 			return
 		}
 
-		// validate the request; if valid, return a Request object
+		// validate the request; if valid, return a pkgRequest object
+		pkgReq, err := parseRequest(msg)
 
 		// here we handle each case (INDEX, QUERY, REMOVE)
 		// using their own functions, each working off the mutex for locking
 		// with pkgTree for storage.
-		pkgReq := strings.Split(req, "|")
-		log.Printf("Received %s request!", req[0])
+
+		log.Printf("Received %s request!", msg[0])
 	}
 }
 
-func parseRequest(req string) (Request, error) {
-	// Check that the request is one of INDEX, QUERY, REMOVE
-	//otherwise return an error
+func parseRequest(msg string) (pkgRequest, error) {
+	var pkgReq pkgRequest
+	validReqs := map[string]struct{}{"INDEX": {}, "QUERY": {}, "REMOVE": {}}
 
-	// For all the package names (both the package and potential dependencies)
-	// Validate with a regular expression
+	msgParts := strings.Split(msg, "|")
+	req := msgParts[0]
+	_, reqOk := validReqs[req]
+
+	if len(msgParts) < 3 || !reqOk {
+		return pkgReq, errors.New("Invalid request")
+	}
+
+	validator, _ := regexp.Compile(`^[a-zA-Z0-9]+[a-zA-Z0-9\-\_\+]*[a-zA-Z0-9\+]*$`)
+
+	// Otherwise, return a request struct
 }
