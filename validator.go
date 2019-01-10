a6073412f68b7a3ac80349042d4409bf561747e2
kawaiian <kawaiian@users.noreply.github.com>
2019-01-09 16:10:50 -0800

    Broke apart main code into helper packages.

--- validator.go
+++ validator.go
@@ -1,29 +1,12 @@
-// Just trying to see how quickly I can stand up a golang package server.
+// Validator package
+// All the code for validating client package requests for the packman server.
 
-package main
+package validator
 
 import (
-	"bufio"
 	"errors"
-	"fmt"
-	"log"
-	"net"
-	"os"
 	"regexp"
 	"strings"
-	"sync"
-)
-
-const (
-	addr     = host + ":" + port
-	connType = "tcp"
-	host     = "0.0.0.0"
-	port     = "8080"
-)
-
-var (
-	mu      sync.RWMutex
-	pkgTree map[string][]string
 )
 
 type pkgRequest struct {
@@ -32,55 +15,8 @@ type pkgRequest struct {
 	depList []string
 }
 
-// --- Main ---
-func main() {
-	lstnr, err := net.Listen(connType, addr)
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
-	defer lstnr.Close()
-	log.Print("Packman server listening on " + addr + "...")
-
-	// Accept connections, concurrently
-	for {
-		conn, err := lstnr.Accept()
-		if err != nil {
-			log.Printf("Error accepting connection: %v\n", err)
-			continue
-		}
-		go handleRequest(conn)
-	}
-}
-
-func handleRequest(c net.Conn) {
-	defer c.Close()
-	log.Printf("Serving %s\n", c.RemoteAddr().String())
-
-	for {
-		msg, err := bufio.NewReader(c).ReadString('\n')
-		if err != nil {
-			log.Printf("Error reading message from client: %v\n", err)
-			return
-		}
-
-		// validate the request
-		pkgReq, err := parsePkgRequest(msg)
-		if err != nil {
-			log.Printf("Invalid request: %v\n", err)
-		} else {
-			switch pkgReq.req {
-			case "INDEX":
-				fmt.Printf("Received INDEX request for %s with dependencies %s\n", pkgReq.pkg, pkgReq.depList)
-			case "QUERY":
-				fmt.Printf("Received QUERY request for %s\n", pkgReq.pkg)
-			case "REMOVE":
-				fmt.Printf("Received REMOVE request for %s\n", pkgReq.pkg)
-			}
-		}
-	}
-}
-
+// ParsePkgRequest validates a package request by decomposing it and
+// passing those components to helper functions.
 func parsePkgRequest(msg string) (pkgRequest, error) {
 	var pkgReq pkgRequest
 
